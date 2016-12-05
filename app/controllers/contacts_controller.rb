418dd6a8432c34c5a22e5aa47c2572d4a1fc51be
Eugene Edgar <tintizzy56@gmail.com>
2016-12-05 09:16:08 +0000

    Fixed typose in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,9 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       name = params[:contact][:name]
-      email = params [:contact][:email]
-      body = params [:contact][:comments]
-      ContactMailer.contact._email(name, email, body).deliver
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message Sent"
       redirect_to new_contact_path
     else
